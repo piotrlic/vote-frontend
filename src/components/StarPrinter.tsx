2115b040e4d9e11f1f2652ab06bc5565512638ca
Piotr Lichwala <piotr.lichwala@digitalnewagency.com>
2023-08-11 15:36:30 +0200

    github pages update

--- src/components/StarPrinter.tsx
+++ src/components/StarPrinter.tsx
@@ -1,17 +1,16 @@
-import {JSX} from "preact";
 
 interface Props {
   n: number;
 }
 
 const StarPrinter = ({ n }) => {
-  const stars: JSX.Element[] = [];
+  const stars = [];
 
   for (let i = 0; i < n; i++) {
     stars.push(<span key={i}>🌟</span>);
   }
 
   return <div>{stars}</div>;
-};
+}
 
 export default StarPrinter;
